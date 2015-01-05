4414c479e4d8fcc6ee85c5077e486d3c375f0a98
SunMicheal <SSun>
2015-01-05 15:00:34 +0800

    Add Archive Data

---
+++ TestExtern/Student.h
@@ -0,0 +1,17 @@
+//
+//  Student.h
+//  TestExtern
+//
+//  Created by keyrun on 15-1-5.
+//  Copyright (c) 2015年 keyrun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject  <NSCoding>
+
+@property (nonatomic ,assign) int age;
+@property (nonatomic ,strong) NSString *name;
+@property (nonatomic ,assign) BOOL sex;
+
+@end
---
+++ TestExtern/Student.m
@@ -0,0 +1,42 @@
+//
+//  Student.m
+//  TestExtern
+//
+//  Created by keyrun on 15-1-5.
+//  Copyright (c) 2015年 keyrun. All rights reserved.
+//
+
+#import "Student.h"
+#define ageKey      @"age"
+#define sexKey      @"sex"
+#define nameKey     @"name"
+@implementation Student
+
+/**
+*  将对象写入文件时调用
+*
+*  @param aCoder
+*/
+- (void)encodeWithCoder:(NSCoder *)aCoder{
+    [aCoder encodeInt:self.age forKey:ageKey];
+    [aCoder encodeObject:self.name forKey:nameKey];
+    [aCoder encodeBool:self.sex forKey:sexKey];
+}
+
+/**
+*  从文件中解析对象时调用
+*
+*  @param aDecoder
+*
+*  @return 对象
+*/
+- (id)initWithCoder:(NSCoder *)aDecoder{
+    if (self == [super init]) {
+        self.age = [aDecoder decodeIntForKey:ageKey];
+        self.name = [aDecoder decodeObjectForKey:nameKey];
+        self.sex = [aDecoder decodeBoolForKey:sexKey];
+    }
+    return self;
+}
+
+@end
